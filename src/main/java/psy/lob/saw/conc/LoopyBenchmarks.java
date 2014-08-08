7a4a333c2eebd04448ff762d054f5ac9ec88940a
nitsanw <nitsanw@yahoo.com>
2014-08-08 19:58:24 +0200

    Add loop benchmarks, move to latest JMH

---
+++ src/main/java/psy/lob/saw/conc/LoopyBenchmarks.java
@@ -0,0 +1,50 @@
+package psy.lob.saw.conc;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Thread)
+public class LoopyBenchmarks {
+	@Param({ "32", "1024", "32768" })
+	int size;
+
+	byte[] bunn;
+
+	@Setup
+	public void prepare() {
+		bunn = new byte[size];
+	}
+
+	@Benchmark
+	public void goodOldLoop(Blackhole fox) {
+		for (int y = 0; y < bunn.length; y++) { // good old C style for (the win?)
+			fox.consume(bunn[y]);
+		}
+	}
+
+	@Benchmark
+	public void sweetLoop(Blackhole fox) {
+		for (byte bunny : bunn) { // syntactic sugar loop goodness
+			fox.consume(bunny);
+		}
+	}
+
+	@Benchmark
+	public void goodOldLoopReturns(Blackhole fox) {
+		byte[] sunn = bunn; // make a local copy of the field
+		for (int y = 0; y < sunn.length; y++) {
+			fox.consume(sunn[y]);
+		}
+	}
+}
