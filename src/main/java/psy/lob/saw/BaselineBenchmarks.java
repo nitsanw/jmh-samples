b081f9e0f723da404da9941037bb7199fc48da06
nitsanw <nitsanw@yahoo.com>
2014-08-22 22:01:51 +0200

    some baseline measures

---
+++ src/main/java/psy/lob/saw/BaselineBenchmarks.java
@@ -0,0 +1,25 @@
+package psy.lob.saw;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Thread)
+public class BaselineBenchmarks {
+	int i;
+	@Benchmark
+	public void noop() {
+	}
+	
+	@Benchmark
+	public void increment() {
+		i++;
+	}
+}
